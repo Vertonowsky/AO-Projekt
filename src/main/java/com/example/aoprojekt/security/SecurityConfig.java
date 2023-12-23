3777585759b028238799cc0ae3f4aa8ce1f412b2
Maciej Sieradz <macieks53@gmail.com>
2023-12-23 11:46:49 +0100

    Naprawienie h2-console

--- src/main/java/com/example/aoprojekt/security/SecurityConfig.java
+++ src/main/java/com/example/aoprojekt/security/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.example.aoprojekt.security;
 
+import static org.springframework.boot.autoconfigure.security.servlet.PathRequest.toH2Console;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
@@ -13,10 +15,13 @@ public class SecurityConfig {
   SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
     http.authorizeHttpRequests(
         c -> {
+          c.requestMatchers(toH2Console()).permitAll();
           c.requestMatchers("/").permitAll().anyRequest().authenticated();
         });
     http.oauth2Login(c -> Customizer.withDefaults());
     http.logout(c -> Customizer.withDefaults());
+    http.csrf(c -> c.ignoringRequestMatchers(toH2Console()));
+    http.headers(c -> c.frameOptions(frame -> frame.disable()));
 
     return http.build();
   }
